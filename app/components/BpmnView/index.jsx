3b0f3c34149f212872df4b1059423c11a92303f4
冉光宇 <593289488@qq.com>
2019-07-17 19:24:32 +0800

    rgy

--- app/components/BpmnView/index.jsx
+++ app/components/BpmnView/index.jsx
@@ -7,11 +7,21 @@
 import "./index.less";
 import React from "react";
 import PropTypes from "prop-types";
+import "svgjs";
 
 export default class BpmnView extends React.Component {
+
+  componentDidMount() {
+    var draw = SVG("bpmn").size(300, 300);
+    draw.rect(50, 100).move(100, 100).attr({fill: '#f06'});
+  }
+
   render() {
     return (
-      <div>流程图</div>
+      <div>
+        <h3>流程图</h3>
+        <div id="bpmn"></div>
+      </div>
     )
   }
 }

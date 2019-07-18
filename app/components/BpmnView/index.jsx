c951a2e76ac8768cb90f7f95384d3a8e27305082
冉光宇 <593289488@qq.com>
2019-07-18 20:01:14 +0800

    动画

--- app/components/BpmnView/index.jsx
+++ app/components/BpmnView/index.jsx
@@ -6,21 +6,41 @@
 
 import "./index.less";
 import React from "react";
-import PropTypes from "prop-types";
-import "svgjs";
+// import PropTypes from "prop-types";
+// import "svgjs";
+import { Motion, spring } from 'react-motion';
 
 export default class BpmnView extends React.Component {
 
+  state = {
+    ways: [
+      { x: 0, y: 0 },
+      { x: 400, y: 100 },
+      { x: 300, y: 200 },
+    ]
+  }
+
   componentDidMount() {
-    var draw = SVG("bpmn").size(300, 300);
-    draw.rect(50, 100).move(100, 100).attr({fill: '#f06'});
+    // var draw = SVG("bpmn").size(300, 300);
+    // draw.rect(50, 100).move(100, 100).attr({fill: "#f06"});
   }
 
   render() {
     return (
       <div>
         <h3>流程图</h3>
-        <div id="bpmn"></div>
+        <div id="bpmn">
+          <Motion
+            defaultStyle={{x: 0, y: 0}}
+            style={{x: spring(400, { precision: 1 }), y: spring(200)}}
+          >
+            {value => (
+              <div
+                className="dot"
+                style={{transform: `translate(${value.x}px,${value.y}px)`}}></div>
+            )}
+          </Motion>
+        </div>
       </div>
     )
   }
